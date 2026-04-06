cd19a801aef6474969b7a18c469b7e784da92293
Henrik Finsberg <henriknf@simula.no>
2026-04-06 21:31:35 +0200

    Fix biv so that is looks more realistic

--- src/cardiac_geometries_core/biv_ellipsoid.py
+++ src/cardiac_geometries_core/biv_ellipsoid.py
@@ -31,7 +31,7 @@ def biv_ellipsoid(
     box_size: float = 15.0,  # Size of the cutting box
     rv_wall_thickness: float = 0.4,  # cm
     lv_wall_thickness: float = 0.5,  # cm
-    rv_offset_x: float = 3.0,
+    rv_offset_x: float = 1.4,
     lv_radius_x: float = 2.0,
     lv_radius_y: float = 1.8,
     lv_radius_z: float = 3.25,
@@ -87,93 +87,64 @@ def biv_ellipsoid(
 
     occ = gmsh.model.occ
 
-    lv_outer_s = occ.addSphere(lv_center[0], lv_center[1], lv_center[2], 1)
-    occ.dilate(
-        [(3, lv_outer_s)],
-        lv_center[0],
-        lv_center[1],
-        lv_center[2],
+    # --- 1. Create Left Ventricle (LV) ---
+    lv_r_inner = (lv_radius_x, lv_radius_y, lv_radius_z)
+    lv_inner = occ.addSphere(lv_center[0], lv_center[1], lv_center[2], 1)
+    occ.dilate([(3, lv_inner)], lv_center[0], lv_center[1], lv_center[2], *lv_r_inner)
+    lv_inner_bounding_box = occ.getBoundingBox(3, lv_inner)
+
+    # Outer LV surface
+    lv_r_outer = (
         lv_radius_x + lv_wall_thickness,
         lv_radius_y + lv_wall_thickness,
         lv_radius_z + lv_wall_thickness,
     )
-    lv_outer_center_of_mass = occ.getCenterOfMass(3, lv_outer_s)
-    lv_outer_bounding_box = occ.getBoundingBox(3, lv_outer_s)
-    logger.debug(f"LV outer: \n{lv_outer_center_of_mass}, \n{lv_outer_bounding_box}")
+    lv_outer = occ.addSphere(lv_center[0], lv_center[1], lv_center[2], 1)
+    occ.dilate([(3, lv_outer)], lv_center[0], lv_center[1], lv_center[2], *lv_r_outer)
+    lv_outer_bounding_box = occ.getBoundingBox(3, lv_outer)
 
-    lv_inner_s = occ.addSphere(lv_center[0], lv_center[1], lv_center[2], 1)
-    occ.dilate(
-        [(3, lv_inner_s)],
-        lv_center[0],
-        lv_center[1],
-        lv_center[2],
-        lv_radius_x,
-        lv_radius_y,
-        lv_radius_z,
-    )
-    lv_inner_center_of_mass = gmsh.model.occ.getCenterOfMass(3, lv_inner_s)
-    lv_inner_bounding_box = gmsh.model.occ.getBoundingBox(3, lv_inner_s)
-    logger.debug(f"LV inner: \n{lv_inner_center_of_mass}, \n{lv_inner_bounding_box}")
+    # We need a copy of the LV outer volume to correctly carve out the RV later
+    lv_outer_copy = occ.copy([(3, lv_outer)])
 
-    rv_outer_s = occ.addSphere(rv_center[0], rv_center[1], rv_center[2], 1)
-    occ.dilate(
-        [(3, rv_outer_s)],
-        rv_center[0],
-        rv_center[1],
-        rv_center[2],
+    # Form the LV wall by cutting the inner cavity from the outer volume
+    lv_wall, _ = occ.cut([(3, lv_outer)], [(3, lv_inner)], removeTool=True, removeObject=True)
+
+    # --- 2. Create Right Ventricle (RV) ---
+    # Inner RV cavity
+    rv_r_inner = (rv_radius_x, rv_radius_y, rv_radius_z)
+    rv_inner = occ.addSphere(rv_center[0], rv_center[1], rv_center[2], 1)
+    occ.dilate([(3, rv_inner)], rv_center[0], rv_center[1], rv_center[2], *rv_r_inner)
+    rv_inner_bounding_box = occ.getBoundingBox(3, rv_inner)
+
+    # Outer RV surface
+    rv_r_outer = (
         rv_radius_x + rv_wall_thickness,
         rv_radius_y + rv_wall_thickness,
         rv_radius_z + rv_wall_thickness,
     )
-    rv_outer_center_of_mass = gmsh.model.occ.getCenterOfMass(3, rv_outer_s)
-    rv_outer_bounding_box = gmsh.model.occ.getBoundingBox(3, rv_outer_s)
-    logger.debug(f"RV outer: \n{rv_outer_center_of_mass}, \n{rv_outer_bounding_box}")
+    rv_outer = occ.addSphere(rv_center[0], rv_center[1], rv_center[2], 1)
+    occ.dilate([(3, rv_outer)], rv_center[0], rv_center[1], rv_center[2], *rv_r_outer)
+    rv_outer_bounding_box = occ.getBoundingBox(3, rv_outer)
 
-    rv_inner_s = occ.addSphere(rv_center[0], rv_center[1], rv_center[2], 1)
-    occ.dilate(
-        [(3, rv_inner_s)],
-        rv_center[0],
-        rv_center[1],
-        rv_center[2],
-        rv_radius_x,
-        rv_radius_y,
-        rv_radius_z,
-    )
-    rv_inner_center_of_mass = gmsh.model.occ.getCenterOfMass(3, rv_inner_s)
-    rv_inner_bounding_box = gmsh.model.occ.getBoundingBox(3, rv_inner_s)
-    logger.debug(f"RV inner: \n{rv_inner_center_of_mass}, \n{rv_inner_bounding_box}")
+    # Form the preliminary RV wall (Full outer - Full inner)
+    rv_wall_full, _ = occ.cut([(3, rv_outer)], [(3, rv_inner)], removeTool=True, removeObject=True)
 
-    lv_wall, _ = occ.cut([(3, lv_outer_s)], [(3, lv_inner_s)])
-    rv_wall, _ = occ.cut([(3, rv_outer_s)], [(3, rv_inner_s)])
+    # --- 3. Create the Crescent/Wrap-around Effect ---
+    # Subtract the LV outer volume from the RV wall.
+    # This removes the overlapping septum and makes the RV attach flush to the LV epicardium.
+    rv_wall_crescent, _ = occ.cut(rv_wall_full, lv_outer_copy, removeTool=True, removeObject=True)
 
-    # Create the myocardium by fusing the LV and RV walls
-    # However this will also contain extra bits inside the cavities, so we will cut those out next
-    myocardium1, _ = occ.fuse(
-        [(3, lv_wall[0][1])],
-        [(3, rv_wall[0][1])],
-        removeTool=True,
-        removeObject=True,
-    )
+    # --- 4. Assemble the Myocardium ---
+    # Fuse the LV wall and the crescent RV wall together into one continuous solid
+    myocardium, _ = occ.fuse(lv_wall, rv_wall_crescent, removeTool=True, removeObject=True)
 
-    # Now cut out the inner cavities of the LV to get the final myocardium with a solid septum
-    lv_inner_s = occ.addSphere(lv_center[0], lv_center[1], lv_center[2], 1)
-    occ.dilate(
-        [(3, lv_inner_s)],
-        lv_center[0],
-        lv_center[1],
-        lv_center[2],
-        lv_radius_x,
-        lv_radius_y,
-        lv_radius_z,
-    )
-    # Here we remove the cells inside the LV cavity only
-    myocardium, _ = occ.cut(
-        [(3, myocardium1[0][1])], [(3, lv_inner_s)], removeTool=False, removeObject=True
-    )
-
-    # Truncate the top to create a flat base
+    # --- 5. Truncate the Base ---
+    # Create a large bounding box to cut off the top part of the ellipsoids (Z > base_cut_z)
+    box_size = 20.0
     trunc_box = occ.addBox(-box_size / 2, -box_size / 2, base_cut_z, box_size, box_size, box_size)
-    final_model, _ = occ.cut([(3, myocardium[0][1])], [(3, trunc_box)])
+
+    # Perform the final cut
+    final_model, _ = occ.cut(myocardium, [(3, trunc_box)], removeTool=True, removeObject=True)
 
     occ.synchronize()
 
@@ -182,10 +153,8 @@ def biv_ellipsoid(
     labels: dict[str, list[int]] = {
         "LV_EPICARDIUM": [],
         "RV_EPICARDIUM": [],
-        "LV_ENDOCARDIUM_FW": [],
-        "LV_ENDOCARDIUM_SEPTUM": [],
-        "RV_ENDOCARDIUM_FW": [],
-        "RV_ENDOCARDIUM_SEPTUM": [],
+        "LV_ENDOCARDIUM": [],
+        "RV_ENDOCARDIUM": [],
         "BASE": [],
     }
 
@@ -208,18 +177,18 @@ def biv_ellipsoid(
 
         if within_bounding_box(bounding_box, lv_inner_bounding_box):
             if center_of_mass[0] < 0:
-                labels["LV_ENDOCARDIUM_FW"].append(s[1])
+                labels["LV_ENDOCARDIUM"].append(s[1])
                 logger.debug("  -> LV free wall endocardium")
             else:
-                labels["LV_ENDOCARDIUM_SEPTUM"].append(s[1])
+                labels["LV_ENDOCARDIUM"].append(s[1])
                 logger.debug("  -> LV septal endocardium")
 
         elif within_bounding_box(bounding_box, rv_inner_bounding_box):
             if center_of_mass[0] > rv_center[0]:
-                labels["RV_ENDOCARDIUM_FW"].append(s[1])
+                labels["RV_ENDOCARDIUM"].append(s[1])
                 logger.debug("  -> RV free wall endocardium")
             else:
-                labels["RV_ENDOCARDIUM_SEPTUM"].append(s[1])
+                labels["RV_ENDOCARDIUM"].append(s[1])
                 logger.debug("  -> RV septal endocardium")
 
         elif within_bounding_box(bounding_box, lv_outer_bounding_box):
@@ -232,22 +201,15 @@ def biv_ellipsoid(
             raise RuntimeError("Surface does not fit any known category")
 
     # Define Physical Groups for different surfaces
-    lv_epi = gmsh.model.addPhysicalGroup(2, labels["LV_EPICARDIUM"])
-    gmsh.model.setPhysicalName(2, lv_epi, "LV_EPI_FW")
-    rv_epi = gmsh.model.addPhysicalGroup(2, labels["RV_EPICARDIUM"])
-    gmsh.model.setPhysicalName(2, rv_epi, "RV_EPI_FW")
+    epi = gmsh.model.addPhysicalGroup(2, labels["LV_EPICARDIUM"] + labels["RV_EPICARDIUM"])
+    gmsh.model.setPhysicalName(2, epi, "EPI")
     base = gmsh.model.addPhysicalGroup(2, labels["BASE"])
     gmsh.model.setPhysicalName(2, base, "BASE")
-    lv_endo_fw = gmsh.model.addPhysicalGroup(2, labels["LV_ENDOCARDIUM_FW"])
-    gmsh.model.setPhysicalName(2, lv_endo_fw, "LV_ENDO_FW")
-    rv_endo_fw = gmsh.model.addPhysicalGroup(2, labels["RV_ENDOCARDIUM_FW"])
-    gmsh.model.setPhysicalName(2, rv_endo_fw, "RV_ENDO_FW")
-    rv_septum = gmsh.model.addPhysicalGroup(2, labels["RV_ENDOCARDIUM_SEPTUM"])
-    gmsh.model.setPhysicalName(2, rv_septum, "RV_SEPTUM")
-    lv_septum = gmsh.model.addPhysicalGroup(2, labels["LV_ENDOCARDIUM_SEPTUM"])
-    gmsh.model.setPhysicalName(2, lv_septum, "LV_SEPTUM")
+    lv_endo_fw = gmsh.model.addPhysicalGroup(2, labels["LV_ENDOCARDIUM"])
+    gmsh.model.setPhysicalName(2, lv_endo_fw, "LV_ENDO")
+    rv_endo_fw = gmsh.model.addPhysicalGroup(2, labels["RV_ENDOCARDIUM"])
+    gmsh.model.setPhysicalName(2, rv_endo_fw, "RV_ENDO")
 
-    # myocardium_group = gmsh.model.addPhysicalGroup(3, [volumes[0][1]])
     myocardium_group = gmsh.model.add_physical_group(dim=3, tags=[t[1] for t in final_model], tag=1)
     gmsh.model.setPhysicalName(3, myocardium_group, "Myocardium")
 
